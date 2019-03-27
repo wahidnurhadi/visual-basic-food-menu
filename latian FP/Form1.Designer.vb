38e0f0901b5331a5d09cdf96fa7ea1751e7c75e4
wahidnurhadi <43665721+wahidnurhadi@users.noreply.github.com>
2019-03-27 23:49:26 +0700

    app

---
+++ latian FP/Form1.Designer.vb
@@ -0,0 +1,394 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BtnMakanan = New System.Windows.Forms.Button()
+        Me.BtnMinuman = New System.Windows.Forms.Button()
+        Me.Btn11 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LblTotal = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Jml11 = New System.Windows.Forms.Button()
+        Me.Btnk11 = New System.Windows.Forms.Button()
+        Me.Btnk31 = New System.Windows.Forms.Button()
+        Me.Jml31 = New System.Windows.Forms.Button()
+        Me.Btn31 = New System.Windows.Forms.Button()
+        Me.Btnk21 = New System.Windows.Forms.Button()
+        Me.Jml21 = New System.Windows.Forms.Button()
+        Me.Btn21 = New System.Windows.Forms.Button()
+        Me.Btnk22 = New System.Windows.Forms.Button()
+        Me.Jml22 = New System.Windows.Forms.Button()
+        Me.Btn22 = New System.Windows.Forms.Button()
+        Me.Btnk32 = New System.Windows.Forms.Button()
+        Me.Jml32 = New System.Windows.Forms.Button()
+        Me.Btn32 = New System.Windows.Forms.Button()
+        Me.Btnk12 = New System.Windows.Forms.Button()
+        Me.Jml12 = New System.Windows.Forms.Button()
+        Me.Btn12 = New System.Windows.Forms.Button()
+        Me.BtnTotal = New System.Windows.Forms.Button()
+        Me.BtnReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnMakanan
+        '
+        Me.BtnMakanan.BackColor = System.Drawing.Color.Coral
+        Me.BtnMakanan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnMakanan.ForeColor = System.Drawing.Color.White
+        Me.BtnMakanan.Location = New System.Drawing.Point(211, 128)
+        Me.BtnMakanan.Name = "BtnMakanan"
+        Me.BtnMakanan.Size = New System.Drawing.Size(169, 63)
+        Me.BtnMakanan.TabIndex = 3
+        Me.BtnMakanan.Text = "Makanan"
+        Me.BtnMakanan.UseVisualStyleBackColor = False
+        '
+        'BtnMinuman
+        '
+        Me.BtnMinuman.BackColor = System.Drawing.Color.Chocolate
+        Me.BtnMinuman.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnMinuman.ForeColor = System.Drawing.Color.White
+        Me.BtnMinuman.Location = New System.Drawing.Point(423, 128)
+        Me.BtnMinuman.Name = "BtnMinuman"
+        Me.BtnMinuman.Size = New System.Drawing.Size(171, 63)
+        Me.BtnMinuman.TabIndex = 4
+        Me.BtnMinuman.Text = "Minuman"
+        Me.BtnMinuman.UseVisualStyleBackColor = False
+        '
+        'Btn11
+        '
+        Me.Btn11.BackColor = System.Drawing.Color.Honeydew
+        Me.Btn11.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.Btn11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn11.Location = New System.Drawing.Point(51, 232)
+        Me.Btn11.Name = "Btn11"
+        Me.Btn11.Size = New System.Drawing.Size(169, 66)
+        Me.Btn11.TabIndex = 5
+        Me.Btn11.Text = "Rendang"
+        Me.Btn11.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(474, 545)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 46)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Rp."
+        '
+        'LblTotal
+        '
+        Me.LblTotal.AutoSize = True
+        Me.LblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTotal.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.LblTotal.Location = New System.Drawing.Point(562, 545)
+        Me.LblTotal.Name = "LblTotal"
+        Me.LblTotal.Size = New System.Drawing.Size(42, 46)
+        Me.LblTotal.TabIndex = 12
+        Me.LblTotal.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Old English Text MT", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(173, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(456, 61)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "RESTAURANT"
+        '
+        'Jml11
+        '
+        Me.Jml11.BackColor = System.Drawing.Color.White
+        Me.Jml11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jml11.Location = New System.Drawing.Point(226, 232)
+        Me.Jml11.Name = "Jml11"
+        Me.Jml11.Size = New System.Drawing.Size(74, 66)
+        Me.Jml11.TabIndex = 14
+        Me.Jml11.UseVisualStyleBackColor = False
+        '
+        'Btnk11
+        '
+        Me.Btnk11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnk11.Image = CType(resources.GetObject("Btnk11.Image"), System.Drawing.Image)
+        Me.Btnk11.Location = New System.Drawing.Point(306, 232)
+        Me.Btnk11.Name = "Btnk11"
+        Me.Btnk11.Size = New System.Drawing.Size(74, 66)
+        Me.Btnk11.TabIndex = 15
+        Me.Btnk11.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Btnk11.UseVisualStyleBackColor = True
+        '
+        'Btnk31
+        '
+        Me.Btnk31.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnk31.Image = CType(resources.GetObject("Btnk31.Image"), System.Drawing.Image)
+        Me.Btnk31.Location = New System.Drawing.Point(306, 410)
+        Me.Btnk31.Name = "Btnk31"
+        Me.Btnk31.Size = New System.Drawing.Size(74, 66)
+        Me.Btnk31.TabIndex = 18
+        Me.Btnk31.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Btnk31.UseVisualStyleBackColor = True
+        '
+        'Jml31
+        '
+        Me.Jml31.BackColor = System.Drawing.Color.White
+        Me.Jml31.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jml31.Location = New System.Drawing.Point(226, 410)
+        Me.Jml31.Name = "Jml31"
+        Me.Jml31.Size = New System.Drawing.Size(74, 66)
+        Me.Jml31.TabIndex = 17
+        Me.Jml31.UseVisualStyleBackColor = False
+        '
+        'Btn31
+        '
+        Me.Btn31.BackColor = System.Drawing.Color.Honeydew
+        Me.Btn31.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.Btn31.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn31.Location = New System.Drawing.Point(51, 410)
+        Me.Btn31.Name = "Btn31"
+        Me.Btn31.Size = New System.Drawing.Size(169, 66)
+        Me.Btn31.TabIndex = 16
+        Me.Btn31.Text = "Lele"
+        Me.Btn31.UseVisualStyleBackColor = False
+        '
+        'Btnk21
+        '
+        Me.Btnk21.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnk21.Image = CType(resources.GetObject("Btnk21.Image"), System.Drawing.Image)
+        Me.Btnk21.Location = New System.Drawing.Point(306, 322)
+        Me.Btnk21.Name = "Btnk21"
+        Me.Btnk21.Size = New System.Drawing.Size(74, 66)
+        Me.Btnk21.TabIndex = 21
+        Me.Btnk21.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Btnk21.UseVisualStyleBackColor = True
+        '
+        'Jml21
+        '
+        Me.Jml21.BackColor = System.Drawing.Color.White
+        Me.Jml21.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jml21.Location = New System.Drawing.Point(226, 322)
+        Me.Jml21.Name = "Jml21"
+        Me.Jml21.Size = New System.Drawing.Size(74, 66)
+        Me.Jml21.TabIndex = 20
+        Me.Jml21.UseVisualStyleBackColor = False
+        '
+        'Btn21
+        '
+        Me.Btn21.BackColor = System.Drawing.Color.Honeydew
+        Me.Btn21.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.Btn21.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn21.Location = New System.Drawing.Point(51, 322)
+        Me.Btn21.Name = "Btn21"
+        Me.Btn21.Size = New System.Drawing.Size(169, 66)
+        Me.Btn21.TabIndex = 19
+        Me.Btn21.Text = "Pechel"
+        Me.Btn21.UseVisualStyleBackColor = False
+        '
+        'Btnk22
+        '
+        Me.Btnk22.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnk22.Image = CType(resources.GetObject("Btnk22.Image"), System.Drawing.Image)
+        Me.Btnk22.Location = New System.Drawing.Point(680, 322)
+        Me.Btnk22.Name = "Btnk22"
+        Me.Btnk22.Size = New System.Drawing.Size(74, 66)
+        Me.Btnk22.TabIndex = 30
+        Me.Btnk22.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Btnk22.UseVisualStyleBackColor = True
+        '
+        'Jml22
+        '
+        Me.Jml22.BackColor = System.Drawing.Color.White
+        Me.Jml22.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jml22.Location = New System.Drawing.Point(600, 322)
+        Me.Jml22.Name = "Jml22"
+        Me.Jml22.Size = New System.Drawing.Size(74, 66)
+        Me.Jml22.TabIndex = 29
+        Me.Jml22.UseVisualStyleBackColor = False
+        '
+        'Btn22
+        '
+        Me.Btn22.BackColor = System.Drawing.Color.Honeydew
+        Me.Btn22.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.Btn22.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn22.Location = New System.Drawing.Point(425, 322)
+        Me.Btn22.Name = "Btn22"
+        Me.Btn22.Size = New System.Drawing.Size(169, 66)
+        Me.Btn22.TabIndex = 28
+        Me.Btn22.Text = "Bubur Ayam"
+        Me.Btn22.UseVisualStyleBackColor = False
+        '
+        'Btnk32
+        '
+        Me.Btnk32.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnk32.Image = CType(resources.GetObject("Btnk32.Image"), System.Drawing.Image)
+        Me.Btnk32.Location = New System.Drawing.Point(680, 410)
+        Me.Btnk32.Name = "Btnk32"
+        Me.Btnk32.Size = New System.Drawing.Size(74, 66)
+        Me.Btnk32.TabIndex = 27
+        Me.Btnk32.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Btnk32.UseVisualStyleBackColor = True
+        '
+        'Jml32
+        '
+        Me.Jml32.BackColor = System.Drawing.Color.White
+        Me.Jml32.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jml32.Location = New System.Drawing.Point(600, 410)
+        Me.Jml32.Name = "Jml32"
+        Me.Jml32.Size = New System.Drawing.Size(74, 66)
+        Me.Jml32.TabIndex = 26
+        Me.Jml32.UseVisualStyleBackColor = False
+        '
+        'Btn32
+        '
+        Me.Btn32.BackColor = System.Drawing.Color.Honeydew
+        Me.Btn32.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.Btn32.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn32.Location = New System.Drawing.Point(425, 410)
+        Me.Btn32.Name = "Btn32"
+        Me.Btn32.Size = New System.Drawing.Size(169, 66)
+        Me.Btn32.TabIndex = 25
+        Me.Btn32.Text = "Seblak"
+        Me.Btn32.UseVisualStyleBackColor = False
+        '
+        'Btnk12
+        '
+        Me.Btnk12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnk12.Image = CType(resources.GetObject("Btnk12.Image"), System.Drawing.Image)
+        Me.Btnk12.Location = New System.Drawing.Point(680, 232)
+        Me.Btnk12.Name = "Btnk12"
+        Me.Btnk12.Size = New System.Drawing.Size(74, 66)
+        Me.Btnk12.TabIndex = 24
+        Me.Btnk12.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Btnk12.UseVisualStyleBackColor = True
+        '
+        'Jml12
+        '
+        Me.Jml12.BackColor = System.Drawing.Color.White
+        Me.Jml12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jml12.Location = New System.Drawing.Point(600, 232)
+        Me.Jml12.Name = "Jml12"
+        Me.Jml12.Size = New System.Drawing.Size(74, 66)
+        Me.Jml12.TabIndex = 23
+        Me.Jml12.UseVisualStyleBackColor = False
+        '
+        'Btn12
+        '
+        Me.Btn12.BackColor = System.Drawing.Color.Honeydew
+        Me.Btn12.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.Btn12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn12.Location = New System.Drawing.Point(425, 232)
+        Me.Btn12.Name = "Btn12"
+        Me.Btn12.Size = New System.Drawing.Size(169, 66)
+        Me.Btn12.TabIndex = 22
+        Me.Btn12.Text = "Nasi Goreng"
+        Me.Btn12.UseVisualStyleBackColor = False
+        '
+        'BtnTotal
+        '
+        Me.BtnTotal.BackColor = System.Drawing.Color.Honeydew
+        Me.BtnTotal.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.BtnTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnTotal.Location = New System.Drawing.Point(275, 540)
+        Me.BtnTotal.Name = "BtnTotal"
+        Me.BtnTotal.Size = New System.Drawing.Size(169, 66)
+        Me.BtnTotal.TabIndex = 31
+        Me.BtnTotal.Text = "Total"
+        Me.BtnTotal.UseVisualStyleBackColor = False
+        '
+        'BtnReset
+        '
+        Me.BtnReset.BackColor = System.Drawing.Color.Honeydew
+        Me.BtnReset.Cursor = System.Windows.Forms.Cursors.SizeAll
+        Me.BtnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnReset.Location = New System.Drawing.Point(52, 540)
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(169, 66)
+        Me.BtnReset.TabIndex = 32
+        Me.BtnReset.Text = "Reset"
+        Me.BtnReset.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Goldenrod
+        Me.ClientSize = New System.Drawing.Size(801, 647)
+        Me.Controls.Add(Me.BtnReset)
+        Me.Controls.Add(Me.BtnTotal)
+        Me.Controls.Add(Me.Btnk22)
+        Me.Controls.Add(Me.Jml22)
+        Me.Controls.Add(Me.Btn22)
+        Me.Controls.Add(Me.Btnk32)
+        Me.Controls.Add(Me.Jml32)
+        Me.Controls.Add(Me.Btn32)
+        Me.Controls.Add(Me.Btnk12)
+        Me.Controls.Add(Me.Jml12)
+        Me.Controls.Add(Me.Btn12)
+        Me.Controls.Add(Me.Btnk21)
+        Me.Controls.Add(Me.Jml21)
+        Me.Controls.Add(Me.Btn21)
+        Me.Controls.Add(Me.Btnk31)
+        Me.Controls.Add(Me.Jml31)
+        Me.Controls.Add(Me.Btn31)
+        Me.Controls.Add(Me.Btnk11)
+        Me.Controls.Add(Me.Jml11)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LblTotal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Btn11)
+        Me.Controls.Add(Me.BtnMinuman)
+        Me.Controls.Add(Me.BtnMakanan)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnMakanan As Button
+    Friend WithEvents BtnMinuman As Button
+    Friend WithEvents Btn11 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblTotal As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Jml11 As Button
+    Friend WithEvents Btnk11 As Button
+    Friend WithEvents Btnk31 As Button
+    Friend WithEvents Jml31 As Button
+    Friend WithEvents Btn31 As Button
+    Friend WithEvents Btnk21 As Button
+    Friend WithEvents Jml21 As Button
+    Friend WithEvents Btn21 As Button
+    Friend WithEvents Btnk22 As Button
+    Friend WithEvents Jml22 As Button
+    Friend WithEvents Btn22 As Button
+    Friend WithEvents Btnk32 As Button
+    Friend WithEvents Jml32 As Button
+    Friend WithEvents Btn32 As Button
+    Friend WithEvents Btnk12 As Button
+    Friend WithEvents Jml12 As Button
+    Friend WithEvents Btn12 As Button
+    Friend WithEvents BtnTotal As Button
+    Friend WithEvents BtnReset As Button
+End Class
---
+++ latian FP/Form1.vb
@@ -0,0 +1,245 @@
+﻿Public Class Form1
+    Dim tet, tj, ts, tab, tej, ted As Integer
+    Dim tr, tng, tp, tba, tl, tsb As Integer
+
+    Private Sub Btnk11_Click(sender As Object, e As EventArgs) Handles Btnk11.Click
+        If (Btn11.Text = "Es Teh") Then
+            tet = tet - 1
+            Jml11.Text = tet
+            If tet <= 0 Then
+                tet = 0
+                Jml11.Text = "0"
+            End If
+        Else
+            tr = tr - 1
+            Jml11.Text = tr
+            If tr <= 0 Then
+                tr = 0
+                Jml11.Text = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub Btnk12_Click(sender As Object, e As EventArgs) Handles Btnk12.Click
+        If (Btn12.Text = "Juice") Then
+            tj = tj - 1
+            Jml12.Text = tj
+            If tj <= 0 Then
+                tj = 0
+                Jml12.Text = "0"
+            End If
+        Else
+            tng = tng - 1
+            Jml12.Text = tng
+            If tng <= 0 Then
+                tng = 0
+                Jml12.Text = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub Btnk21_Click(sender As Object, e As EventArgs) Handles Btnk21.Click
+        If (Btn21.Text = "Susu") Then
+            ts = ts - 1
+            Jml21.Text = ts
+            If ts <= 0 Then
+                ts = 0
+                Jml21.Text = "0"
+            End If
+        Else
+            tp = tp - 1
+            Jml21.Text = tp
+            If tp <= 0 Then
+                tp = 0
+                Jml21.Text = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub Btnk22_Click(sender As Object, e As EventArgs) Handles Btnk22.Click
+        If (Btn22.Text = "Air Bening") Then
+            tab = tab - 1
+            Jml22.Text = tab
+            If tab <= 0 Then
+                tab = 0
+                Jml22.Text = "0"
+            End If
+        Else
+            tba = tba - 1
+            Jml22.Text = tba
+            If tba <= 0 Then
+                tba = 0
+                Jml22.Text = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub Btnk31_Click(sender As Object, e As EventArgs) Handles Btnk31.Click
+        If (Btn31.Text = "Es Jeruk") Then
+            tej = tej - 1
+            Jml31.Text = tej
+            If tej <= 0 Then
+                tej = 0
+                Jml31.Text = "0"
+            End If
+        Else
+            tl = tl - 1
+            Jml31.Text = tl
+            If tl <= 0 Then
+                tl = 0
+                Jml31.Text = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub Btnk32_Click(sender As Object, e As EventArgs) Handles Btnk32.Click
+        If (Btn32.Text = "Es Degan") Then
+            ted = ted - 1
+            Jml32.Text = ted
+            If ted <= 0 Then
+                ted = 0
+                Jml32.Text = "0"
+            End If
+        Else
+            tsb = tsb - 1
+            Jml32.Text = tsb
+            If tsb <= 0 Then
+                tsb = 0
+                Jml32.Text = "0"
+            End If
+        End If
+    End Sub
+
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        tet = 0
+        tj = 0
+        ts = 0
+        tab = 0
+        tej = 0
+        ted = 0
+        tr = 0
+        tng = 0
+        tp = 0
+        tba = 0
+        tl = 0
+        tsb = 0
+        LblTotal.Text = 0
+        If (Btn11.Text = "Es Teh") Then
+            Jml11.Text = tet
+            Jml12.Text = tj
+            Jml21.Text = ts
+            Jml22.Text = tab
+            Jml31.Text = tej
+            Jml32.Text = ted
+        Else
+            Jml11.Text = tr
+            Jml12.Text = tng
+            Jml21.Text = tp
+            Jml22.Text = tba
+            Jml31.Text = tl
+            Jml32.Text = tsb
+        End If
+    End Sub
+
+    Dim TotalMakan, TotalMinum, Total As Integer
+
+    Private Sub Btn21_Click(sender As Object, e As EventArgs) Handles Btn21.Click
+        If (Btn21.Text = "Susu") Then
+            ts = ts + 1
+            Jml21.Text = ts
+        Else
+            tp = tp + 1
+            Jml21.Text = tp
+        End If
+    End Sub
+
+    Private Sub Btn22_Click(sender As Object, e As EventArgs) Handles Btn22.Click
+        If (Btn22.Text = "Air Bening") Then
+            tab = tab + 1
+            Jml22.Text = tab
+        Else
+            tba = tba + 1
+            Jml22.Text = tba
+        End If
+    End Sub
+
+    Private Sub Btn31_Click(sender As Object, e As EventArgs) Handles Btn31.Click
+        If (Btn31.Text = "Es Jeruk") Then
+            tej = tej + 1
+            Jml31.Text = tej
+        Else
+            tl = tl + 1
+            Jml31.Text = tl
+        End If
+    End Sub
+
+    Private Sub Btn32_Click(sender As Object, e As EventArgs) Handles Btn32.Click
+        If (Btn32.Text = "Es Degan") Then
+            ted = ted + 1
+            Jml32.Text = ted
+        Else
+            tsb = tsb + 1
+            Jml32.Text = tsb
+        End If
+    End Sub
+
+    Private Sub BtnTotal_Click(sender As Object, e As EventArgs) Handles BtnTotal.Click
+        TotalMakan = (tr * 12000) + (tng * 10000) + (tp * 7000) + (tba * 6000) + (tl * 7000) + (tsb * 8000)
+        TotalMinum = (tet * 3000) + (tj * 5000) + (ts * 4000) + (tab * 2000) + (tej * 3000) + (ted * 4000)
+        Total = TotalMakan + TotalMinum
+        LblTotal.Text = Total
+    End Sub
+
+    Private Sub Btn11_Click(sender As Object, e As EventArgs) Handles Btn11.Click
+        If (Btn11.Text = "Es Teh") Then
+            tet = tet + 1
+            Jml11.Text = tet
+        Else
+            tr = tr + 1
+            Jml11.Text = tr
+        End If
+    End Sub
+    Private Sub Btn12_Click(sender As Object, e As EventArgs) Handles Btn12.Click
+        If (Btn12.Text = "Juice") Then
+            tj = tj + 1
+            Jml12.Text = tj
+        Else
+            tng = tng + 1
+            Jml12.Text = tng
+        End If
+    End Sub
+
+
+    Private Sub BtnMakanan_Click(sender As Object, e As EventArgs) Handles BtnMakanan.Click
+        Btn11.Text = "Rendang"
+        Btn12.Text = "Nasi Goreng"
+        Btn21.Text = "Pechel"
+        Btn22.Text = "Bubur Ayam"
+        Btn31.Text = "Lele"
+        Btn32.Text = "Seblak"
+        Jml11.Text = tr
+        Jml12.Text = tng
+        Jml21.Text = tp
+        Jml22.Text = tba
+        Jml31.Text = tl
+        Jml32.Text = tsb
+    End Sub
+
+    Private Sub BtnMinuman_Click(sender As Object, e As EventArgs) Handles BtnMinuman.Click
+        Btn11.Text = "Es Teh"
+        Btn12.Text = "Juice"
+        Btn21.Text = "Susu"
+        Btn22.Text = "Air Bening"
+        Btn31.Text = "Es Jeruk"
+        Btn32.Text = "Es Degan"
+        Jml11.Text = tet
+        Jml12.Text = tj
+        Jml21.Text = ts
+        Jml22.Text = tab
+        Jml31.Text = tej
+        Jml32.Text = ted
+
+    End Sub
+
+End Class
+
